1abc56dee45723f7f7cd1245ec1ad5b57f2e1514
Michele <michelegec@gmail.com>
2025-03-08 19:21:23 +0100

    Commit

--- app/page.js
+++ app/page.js
@@ -1,22 +1,15 @@
 import { getSortedPosts } from "../lib/posts";
 
-export async function getStaticProps() {
+export default async function Page() {
   const posts = getSortedPosts();
-  return { props: { posts } };
-}
 
-export default function Home({ posts }) {
   return (
-    <div className="max-w-2xl mx-auto p-4">
-      <h1 className="text-3xl font-bold">Blog</h1>
+    <div>
+      <h1>Blog</h1>
       <ul>
         {posts.map((post) => (
-          <li key={post.slug} className="mt-4">
-            <a href={`/post/${post.slug}`} className="text-blue-500">
-              {post.title}
-            </a>
-            <p className="text-gray-500 text-sm">{post.date}</p>
-            <p>{post.excerpt}</p>
+          <li key={post.id}>
+            <a href={`/post/${post.slug}`}>{post.title}</a>
           </li>
         ))}
       </ul>
